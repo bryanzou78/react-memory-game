fe9995a457a2e42be5ce3f47a0e7011bd0dbc04c
Bryan Zou <175367072+bryanzou78@users.noreply.github.com>
2024-12-09 14:01:21 -0800

    Add styling for buttons to show active and inactive mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,32 @@ const Header = ({ gameStatus, score, onNormalReset, onExtremeReset }) => {
         <div className='header-container'>
             <h1>Memory Game</h1>
             {gameStatus === 'won' && (
-                <h2>You won! Play again?</h2>
+                <div>
+                    <h2>You won! Play again?</h2>
+                    <button className='reset-btn normal-btn' onClick={onNormalReset}>Normal</button>
+                    <button className='reset-btn extreme-btn' onClick={onExtremeReset}>Extreme</button>
+                </div>
             )}
             {gameStatus === 'lost' && (
-                <h2>Game Over! Your score was {score}. Try again?</h2>
+                <div>
+                    <h2>Game Over! Your score was {score}. Try again?</h2>
+                    <button className='reset-btn normal-btn' onClick={onNormalReset}>Normal</button>
+                    <button className='reset-btn extreme-btn' onClick={onExtremeReset}>Extreme</button>
+                </div>
             )}
-            <button className='reset-btn normal-btn' onClick={onNormalReset}>Normal</button>
-            <button className='reset-btn extreme-btn' onClick={onExtremeReset}>Extreme</button>
-            {(gameStatus === 'playingNormal' || gameStatus === 'playingExtreme') && (
+            {gameStatus === 'playingNormal' && (
+                <div>
                     <h2>Score: {score}</h2>
+                    <button className='reset-btn normal-btn normal-btn-active' onClick={onNormalReset}>Normal</button>
+                    <button className='reset-btn extreme-btn extreme-btn-inactive' onClick={onExtremeReset}>Extreme</button>
+                </div>
+            )}
+            {gameStatus === 'playingExtreme' && (
+                <div>
+                    <h2>Score: {score}</h2>
+                    <button className='reset-btn normal-btn normal-btn-inactive' onClick={onNormalReset}>Normal</button>
+                    <button className='reset-btn extreme-btn extreme-btn-active' onClick={onExtremeReset}>Extreme</button>
+                </div>
             )}
         </div>
     )
